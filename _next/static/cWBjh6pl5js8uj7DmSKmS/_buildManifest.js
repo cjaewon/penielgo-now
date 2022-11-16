b40768ab1aa2ca7cdb6b01d73489795e1024cd48
cjaewon <cjaewon@users.noreply.github.com>
2022-11-16 03:48:59 +0000

    Deploying to gh-pages from @ cjaewon/penielgo-now@8dadab55d33e49950b7b1de478a1daeb069889dd 🚀

---
+++ _next/static/cWBjh6pl5js8uj7DmSKmS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/22f102f7f66cd62b.css","static/chunks/pages/index-05c7ba5371f6abee.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/sCrEG9eAfxGkxh9ZxcdA8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/48dac6480cbac250.css","static/chunks/pages/index-ae9af86349f422c9.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
