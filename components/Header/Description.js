e970dfac365a7c6fd3ba942994fe3f1302b47307
cjaewon <cjaewon@users.noreply.github.com>
2022-11-16 02:27:58 +0000

    Deploying to gh-pages from @ cjaewon/penielgo-now@41fcd0acb4a9d374d5d143e5770e375f58bd785f 🚀

--- components/Header/Description.js
+++
@@ -1,9 +0,0 @@
-import styles from "./styles/Description.module.css";
-
-export default function Description({ text }) {
-  return (
-    <div>
-      <p className={styles.text}>{text}</p>    
-    </div>
-  )
-}
